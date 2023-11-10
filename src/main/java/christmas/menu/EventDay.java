d8e112301d5edb1bc57cf8f3b27a1f58bee40982
RESD\dkanf <dkanfjgwls@naver.com>
2023-11-10 17:14:45 +0900

    feat: Error메시지와 이벤트 날짜를 저장한 enum 클래스 생성

---
+++ src/main/java/christmas/menu/EventDay.java
@@ -0,0 +1,16 @@
+package christmas.menu;
+
+public enum EventDay {
+    YEAR(2023),
+    MONTH(12);
+
+    private final int value;
+
+    EventDay(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
