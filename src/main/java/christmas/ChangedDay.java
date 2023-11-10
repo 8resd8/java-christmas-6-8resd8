5f5e3a21d27821000b8d3b9f0e0c01fb0f6f443e
RESD\dkanf <dkanfjgwls@naver.com>
2023-11-10 17:08:57 +0900

    docs: README 업데이트 - 날짜 요일값 반환 내용 작성 feat: 날짜 입력에 따라 일요일 ~ 토요일을 반환하는 클래스 구현

---
+++ src/main/java/christmas/ChangedDay.java
@@ -0,0 +1,24 @@
+package christmas;
+
+import christmas.menu.EventDay;
+
+import java.util.Calendar;
+
+public class ChangedDay {
+    Calendar calendar;
+
+    public int getDay(int userDay) {
+        calendar = Calendar.getInstance();
+        int year = EventDay.YEAR.getValue();
+        int month = EventDay.MONTH.getValue();
+        calendar.set(year, month - 1, userDay); // 설정 : 2023년 12월 사용자가 입력한 일
+        return getDayOfWeek(calendar);
+    }
+
+    private int getDayOfWeek(Calendar calendar) {
+        // 요일을 숫자로 가져오고, 숫자를 1부터 7로 변환
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        return (dayOfWeek + 5) % 7 + 1; // 1부터 7로 변환
+
+    }
+}
