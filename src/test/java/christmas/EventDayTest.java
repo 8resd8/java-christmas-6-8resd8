2790fad68599244e155e1e81b262664fc181151c
RESD\dkanf <dkanfjgwls@naver.com>
2023-11-13 20:46:01 +0900

    docs: EventDay 테스트 내용 추가 test: 요구 사항 값과 상수 값 일치 확인 테스트 코드 작성

---
+++ src/test/java/christmas/EventDayTest.java
@@ -0,0 +1,23 @@
+package christmas;
+
+import christmas.EventDay;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventDayTest {
+
+    @Test
+    @DisplayName("이벤트 2023년도 12월 가져오기 테스트")
+    public void testGetEventDate() {
+        EventDay year = EventDay.YEAR;
+        EventDay month = EventDay.MONTH;
+
+        int eventYear = year.getValue();
+        int eventMonth = month.getValue();
+
+        assertEquals(2023, eventYear);
+        assertEquals(12, eventMonth);
+    }
+}
